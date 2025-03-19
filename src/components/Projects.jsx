42ed4576b2d906fdb834550abfbfe05131e97ae3
218R1A0567 <akshithraj5003@gmail.com>
2025-03-19 22:53:48 +0530

    Projects components is done data.json is added for data storing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,50 @@
-function Projects(props) {
-  return <h1 className="display-3">PROJECTS</h1>;
+import data from "./assets/data.json";
+import "./projects.css";
+
+function Projects() {
+  return (
+    <>
+      <h1 className="display-3 text-center">
+        <strong>PROJECTS</strong>
+      </h1>
+      <div className="container">
+        {data.Projects.map((project) => (
+          <div key={project.id} className="container-x card p-3 mb-4 shadow">
+            <div className="row">
+              <div className="col-6 col-md-4 col-lg-3">
+                <strong className="fs-4">TITLE</strong>
+              </div>
+              <div className="col-6 col-md-8 col-lg-9 fs-4">{project.name}</div>
+            </div>
+            <div className="row">
+              <div className="col-6 col-md-4 col-lg-3">
+                <strong className="fs-4">DESCRIPTION</strong>
+              </div>
+              <div className="col-6 col-md-8 col-lg-9 fs-5">
+                {project.description}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6 col-md-4 col-lg-3">
+                <strong className="fs-4">TECHNOLOGIES</strong>
+              </div>
+              <div className="col-6 col-md-8 col-lg-9 fs-5">{project.Tech}</div>
+            </div>
+            <div className="row">
+              <div className="col-6 col-md-4 col-lg-3">
+                <strong className="fs-4">LINK</strong>
+              </div>
+              <div className="col-6 col-md-8 col-lg-9 fs-5">
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  {project.url}
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Projects;
